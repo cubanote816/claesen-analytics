f39658f762580b6fad9048fe46ac09042e0497a9
Orelvys <cubanote816@gmail.com>
2026-02-09 21:16:25 +0100

    feat(website): remove redundant Page resource and functionality

--- Modules/Website/Routes/api.php
+++ Modules/Website/Routes/api.php
@@ -1,14 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Website\Http\Controllers\PageController;
 use Modules\Website\Http\Controllers\ProjectController;
 use Modules\Website\Http\Controllers\ConsultationController;
 
 Route::prefix('v1/website')->group(function () {
-    Route::get('/pages', [PageController::class, 'index']);
-    Route::get('/pages/{slug}', [PageController::class, 'show']);
-
     Route::get('/projects', [ProjectController::class, 'index']);
     Route::get('/projects/categories', [ProjectController::class, 'categories']);
     Route::get('/projects/years', [ProjectController::class, 'years']);
