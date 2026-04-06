c19f7f4a4d332623ff5d515bd71474e62c897ca8
Orelvys <cubanote816@gmail.com>
2026-04-06 14:21:30 +0200

    refactor: remove legacy web routes from Mailing and Prospects modules to resolve Filament path conflicts

--- Modules/Mailing/routes/web.php
+++ Modules/Mailing/routes/web.php
@@ -4,9 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Modules\Mailing\Http\Controllers\MailingController;
 use Modules\Mailing\Http\Controllers\UnsubscribeController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('mailings', MailingController::class)->names('mailing');
-});
+// Removed legacy web route to avoid conflict with Filament root path
+
 
 // Public Unsubscribe Routes
 Route::get('prospects/unsubscribe/{prospect}/{token}', [UnsubscribeController::class, 'show'])->name('prospects.unsubscribe');
--- Modules/Prospects/routes/web.php
+++ Modules/Prospects/routes/web.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Prospects\Http\Controllers\ProspectsController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('prospects', ProspectsController::class)->names('prospects');
-});
+// Removed legacy web route to avoid conflict with Filament root path
+
